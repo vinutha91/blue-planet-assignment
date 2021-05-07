d9a9e811b23047a3a4e29e646611d4c645fa8159
Vinutha Govindaraju <vinu.raju@gmail.com>
2021-05-07 17:58:47 +0530

    [Header] | Add blue-planet-header - Generated a blue-planet header via nx - Added it in module dependency

--- apps/blue-planet/src/app/app.module.ts
+++ apps/blue-planet/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
