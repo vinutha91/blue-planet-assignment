6d6f49ef790e1ce74e6e688fd7d83116f7dde0d0
Vinutha Govindaraju <vinu.raju@gmail.com>
2021-05-09 10:23:09 +0530

    [spec] | cover tests for ActionBarComponent

--- apps/blue-planet/src/app/action-bar/action-bar.component.spec.ts
+++ apps/blue-planet/src/app/action-bar/action-bar.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockChildComponent } from '../test-util/mock-child.component';
 
 import { ActionBarComponent } from './action-bar.component';
 
@@ -8,9 +9,19 @@ describe('ActionBarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ActionBarComponent ]
+      declarations: [
+        ActionBarComponent,
+        MockChildComponent({
+          selector: 'p-dropdown',
+          inputs: ['placeholder', 'class', 'options', 'formControlName']
+        }),
+        MockChildComponent({
+          selector: 'div',
+          inputs: ['formGroup']
+        })
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -19,7 +30,38 @@ describe('ActionBarComponent', () => {
     fixture.detectChanges();
   });
 
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a method homeClick which calls emit method of onHomeClick event emitter', () => {
+    const emitSpy = jest.spyOn(
+      component.onHomeClick,
+      'emit'
+    );
+    component.homeClick();
+    expect(emitSpy).toHaveBeenCalled();
+  });
+
+  it('should have a method backClick which calls emit method of onBackClick event emitter', () => {
+    const emitSpy = jest.spyOn(
+      component.onBackClick,
+      'emit'
+    );
+    component.backClick();
+    expect(emitSpy).toHaveBeenCalled();
+  });
+
+  it('should have a method optionSelected which calls emit method of onOptionSelected event emitter', () => {
+    const emitSpy = jest.spyOn(
+      component.onOptionSelected,
+      'emit'
+    );
+    component.optionSelected({});
+    expect(emitSpy).toHaveBeenCalled();
   });
 });
